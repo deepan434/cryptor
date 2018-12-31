bb5205fe87507313d9ea3e6b8a0f4f3626dc851a
Deepan Chakravarthi <39833033+deepan434@users.noreply.github.com>
2018-12-31 14:48:59 +0530

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -17,6 +17,78 @@ def decryptor(phrase):
               translation = translation + "d"
         elif letter.lower() in "π":
               translation = translation + "e"
+        elif letter.lower() in "8":
+
+              translation = translation + "c"
+
+        elif letter.lower() in "#":
+
+              translation = translation + "f"
+
+        elif letter.lower() in "$":
+
+              translation = translation + "g"
+
+        elif letter.lower() in "ww":
+
+              translation = translation + "h"
+
+        elif letter.lower() in "0":
+
+              translation = translation + "j"
+
+        elif letter.lower() in "(":
+
+              translation = translation + "k"
+
+        elif letter.lower() in "}":
+
+              translation = translation + "l"
+
+        elif letter.lower() in "5":
+
+              translation = translation + "m"
+
+        elif letter.lower() in "!":
+
+              translation = translation + "n"
+
+        elif letter.lower() in "?":
+
+              translation = translation + "p"
+
+        elif letter.lower() in "3":
+
+              translation = translation + "r"
+
+        elif letter.lower() in "[":
+
+              translation = translation + "s"
+
+        elif letter.lower() in "{":
+
+              translation = translation + "t"
+
+        elif letter.lower() in ")":
+
+              translation = translation + "v"
+
+        elif letter.lower() in "6":
+
+              translation = translation + "w"
+
+        elif letter.lower() in "]":
+
+              translation = translation + "x"
+
+        elif letter.lower() in "7":
+
+              translation = translation + "y"
+
+        elif letter.lower() in "4":
+
+              translation = translation + "z"
+
         else:
               translation = translation + letter
      return translation
