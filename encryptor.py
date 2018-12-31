ee5f10ccc54b3c643fd539befe2b9f12ec9f1c9b
Deepan Chakravarthi <39833033+deepan434@users.noreply.github.com>
2018-12-31 14:37:56 +0530

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -20,75 +20,75 @@ def encryptor(phrase):
            
         elif letter.lower() in "c":
 
-             translation = translation + "8"
+              translation = translation + "8"
 
         elif letter.lower() in "f":
 
-             translation = translation + "#"
+              translation = translation + "#"
 
         elif letter.lower() in "g":
 
-             translation = translation + "$"
+              translation = translation + "$"
 
         elif letter.lower() in "h":
 
-             translation = translation + "ww"
+              translation = translation + "ww"
 
         elif letter.lower() in "j":
 
-             translation = translation + "0"
+              translation = translation + "0"
 
         elif letter.lower() in "k":
 
-             translation = translation + "("
+              translation = translation + "("
 
         elif letter.lower() in "l":
 
-             translation = translation + "}"
+              translation = translation + "}"
 
         elif letter.lower() in "m":
 
-             translation = translation + "5"
+              translation = translation + "5"
 
         elif letter.lower() in "n":
 
-             translation = translation + "!"
+              translation = translation + "!"
 
         elif letter.lower() in "p":
 
-             translation = translation + "?"
+              translation = translation + "?"
 
         elif letter.lower() in "r":
 
-             translation = translation + "3"
+              translation = translation + "3"
 
         elif letter.lower() in "s":
 
-             translation = translation + "["
+              translation = translation + "["
 
         elif letter.lower() in "t":
 
-             translation = translation + "{"
+              translation = translation + "{"
 
         elif letter.lower() in "v:
 
-             translation = translation + ")"
-
+              translation = translation + ")"
+ 
         elif letter.lower() in "w":
 
-             translation = translation + "6"
+              translation = translation + "6"
 
         elif letter.lower() in "x":
 
-             translation = translation + "]"
+              translation = translation + "]"
 
         elif letter.lower() in "y":
 
-             translation = translation + "7"
+              translation = translation + "7"
 
         elif letter.lower() in "z":
 
-             translation = translation + "4"
+              translation = translation + "4"
         else:
               translation = translation + letter
      return translation
